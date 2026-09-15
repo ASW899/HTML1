acae97e016c6ccb0f60fe14a01c54cfe6e8e0f25
ASW899 <aswinsree3397@gmail.com>
2026-09-15 13:16:17 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title> JS 1 </title>
+<script>
+function Myfunc(){
+document.getElementById("demo").innerHTML="Paragraph changed";
+}
+</script>
+</head>
+<body>
+<h2> DEMO JS IN HEAD </h2>
+<p id="demo"> A paragraph </p>
+<button type="button" onclick="Myfunc()"> Try </button>
+</body>
+</html>
+
